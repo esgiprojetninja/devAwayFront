9e27095bec61dad4338a804a77ac726b505a889e
dydyFos <dylanfoster.esgi@gmail.com>
2017-11-24 16:16:23 +0100

    eslint setup

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,14 @@
-import BasePage from "../components/HOC/BasePage";
 import React from "react";
+import BasePage from "../components/HOC/BasePage";
 import * as types from "../server/lib/config/types";
 
 class Index extends React.Component {
-
     static getInitialProps(context) {
         return {
             user: context.user
         };
     }
-
+    
     render() {
         return (
             <h1>Hello {this.props.user.displayName ? this.props.user.displayName : ""} !</h1>
