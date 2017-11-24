3b92064a0297794729203c9762b358487e3df609
Roland Kullet <renaud.bellec.3@gmail.com>
2017-11-24 14:06:15 +0100

    added coverage

---
+++ nex.config.js
@@ -0,0 +1,15 @@
+/* eslint-env node */
+module.exports = {
+    webpack: (config, {buildId, dev}) => {
+        // Perform customizations to webpack config
+
+        // Important: return the modified config
+        return config;
+    },
+    webpackDevMiddleware: config => {
+        // Perform customizations to webpack dev middleware config
+
+        // Important: return the modified config
+        return config;
+    }
+};
