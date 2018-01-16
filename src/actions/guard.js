8f10e1226374ce5b815859d335ef60fea004518a
Roland Kullet <renaud.bellec.3@gmail.com>
2018-01-17 00:09:50 +0100

    Guard actions and reducer

---
+++ src/actions/guard.js
@@ -0,0 +1,41 @@
+export const UPDATE_CREDENTIALS = "UPDATE_CREDENTIALS";
+export const updateCredentials = (property, value) => ({
+    type: UPDATE_CREDENTIALS,
+    payload: {
+        property,
+        value
+    }
+});
+
+export const CHECK_GUARD_REQUEST = "CHECK_GUARD_REQUEST";
+const checkGuardRequest = () => ({
+    type: CHECK_GUARD_REQUEST
+});
+
+export const CHECK_GUARD_SUCCESS = "CHECK_GUARD_SUCCESS";
+const checkGuardSuccess = payload => ({
+    type: CHECK_GUARD_SUCCESS,
+    payload
+});
+
+export const CHECK_GUARD_FAILURE = "CHECK_GUARD_FAILURE";
+const checkGuardFailure = payload => ({
+    type: CHECK_GUARD_FAILURE,
+    payload
+});
+
+export function checkGuard() {
+    return (dispatch, getState, API) => {
+        dispatch(checkGuardRequest());
+        return API.guardApi.checkGuard(getState().data)
+            .then(
+                (res) => {
+                    if (res.hasError) {
+                        return dispatch(checkGuardFailure(res.message));
+                    }
+                    return dispatch(checkGuardSuccess(res));
+                },
+                error => console.log(error)
+            );
+    };
+}
---
+++ src/reducers/guard.js
@@ -0,0 +1,52 @@
+import {
+    UPDATE_CREDENTIALS,
+    CHECK_GUARD_REQUEST,
+    CHECK_GUARD_SUCCESS,
+    CHECK_GUARD_FAILURE
+} from "../actions/guard";
+
+const initialSate = {
+    data: {},
+    isLoading: false,
+    hasError: false,
+    errorText: ""
+};
+
+const guard = (state = initialSate, action) => {
+    const { payload } = action;
+    switch (action.type) {
+    case UPDATE_CREDENTIALS:
+        return {
+            ...state,
+            data: {
+                ...state.data,
+                [payload.property]: payload.value
+            }
+        };
+    case CHECK_GUARD_REQUEST:
+        return {
+            ...state,
+            isLoading: true
+        };
+    case CHECK_GUARD_SUCCESS:
+        return {
+            ...state,
+            isLoading: false,
+            data: {
+                ...state.data,
+                code: payload
+            }
+        };
+    case CHECK_GUARD_FAILURE:
+        return {
+            ...state,
+            isLoading: false,
+            hasError: true,
+            errorText: payload
+        };
+    default:
+        return state;
+    }
+};
+
+export default guard;
